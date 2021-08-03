091bdd81df2ca1e118d7a95a2e37321a718d6c64
kbrycz <kbrycz@umich.edu>
2021-08-03 00:19:00 -0500

    Adding python files

---
+++ starter.py
@@ -0,0 +1,38 @@
+import csv
+import json
+
+# Final dictionary that holds all the players
+playerDict = {}
+
+# Opens csv file and reads in the data line by line
+with open('players.csv') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    next(csv_file)
+    team = ''
+
+    # Gets player info
+    for row in csv_reader:
+        if row[0] != '':
+            team = row[0]
+        else:
+            name = row[1]
+            number = row[2]
+            score = row[7]
+
+            # Create the key in the format 'matthewstaffordLAR9'
+            key = name.replace(" ", "").lower() + team + number
+
+            # Create the data object and add to playerDict
+            obj = {
+                'name': name,
+                'team': team,
+                'number': number,
+                'score': score,
+            }
+
+            playerDict[key] = obj
+
+# Write the data to a text file
+f = open("playerDict.txt", "w")
+f.write(json.dumps(playerDict, indent=4))
+f.close()
